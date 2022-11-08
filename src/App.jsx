c04ef4fbacbd8d188b81d6e36ff8a9c59393bcfd
DireWolf707 <rahulsood707@gmail.com>
2022-11-08 22:55:32 +0530

    added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header"
+
 function App() {
-  return <div className="text-lg">Hello world</div>
+  return (
+    <div className="mx-2">
+      <Header />
+      <div className="text-lg">Hello world</div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return <div className="bg-indigo-700 p-4 rounded-b-md">
+    <a className="font-bold font-serif text-3xl text-cyan-100" href="/">IpuShare</a>
+  </div>
+}
+
+export default Header
